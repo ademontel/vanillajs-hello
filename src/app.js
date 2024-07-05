752e9da5e6898386d538b05f02ddb77dbca3c1eb
ademontel <agustindemontel@gmail.com>
2024-07-05 12:54:27 -0300

    Se cambia el index, app y styles

--- src/app.js
+++ src/app.js
@@ -2,10 +2,53 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
+/*import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+*/
+
+let who = [
+  "The dog",
+  "My grandma",
+  "The mailman",
+  "John Lennon",
+  "Zombie Jesus",
+  "Joe Biden"
+];
+let action = ["ate", "peed", "crushed", "broke", "send to China", "Exploded"];
+let what = [
+  "my homework",
+  "my phone",
+  "the car",
+  "my laptop",
+  "my dignity",
+  "my ideas"
+];
+let when = [
+  "before class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying",
+  "when I was in the toilet"
+];
+
+function getRandomInt() {
+  let randomNumber = Math.floor(10 * Math.random());
+  while (randomNumber > 5) {
+    randomNumber = Math.floor(10 * Math.random());
+  }
+  return randomNumber;
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let frase =
+    who[getRandomInt()] +
+    " " +
+    action[getRandomInt()] +
+    " " +
+    what[getRandomInt()] +
+    " " +
+    when[getRandomInt()];
+  document.getElementById("excuse").innerHTML = frase;
 };
